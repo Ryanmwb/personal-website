7ac0dbfd4e5b335ef5e73e00f2632bd34f9e3887
Ryan <ryanmwb@gmail.com>
2019-01-12 13:44:52 -0500

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import {Route, Link} from 'react-router-dom';
 import './App.css';
-import TopNavigation from './components/TopNavigation'
 import Test from './components/Test'
+import Landing from './components/Landing'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <Route path="/" component={TopNavigation} />
-          <Route path="/test" component={Test} />
+          <div > 
+            <nav class="navbar navbar-expand lg navbar-light bg-light fixed-top">
+                <a class="navbar-brand display-6" href="/">Ryan's Site</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <Link to="/test">Test</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+          </div>
         </header>
+        <main>
+          <Route exact path="/" component={Landing}/>
+          <Route path="/test" component={Test}/>
+        </main>
       </div>
     );
   }
