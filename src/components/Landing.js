02ed194b7af8a90d591d9609b7108a9eddd5fd17
Ryan <ryanmwb@gmail.com>
2019-01-13 16:39:23 -0500

    sizing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ class Landing extends Component {
         return(
             <div class="landing">
                 <div class="color">
-                    <h1 class="display-1 pt-5">Ryan Wilhelm</h1>
+                    <h1 class="display-2 pt-5">Ryan Wilhelm</h1>
                 </div>
             </div>
         )
