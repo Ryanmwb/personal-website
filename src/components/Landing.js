8962374192abb7a9356f64d7febe9ddd78235594
Ryan <ryanmwb@gmail.com>
2019-01-13 19:42:06 -0500

    mobile title-size

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,7 @@ class Landing extends Component {
             <div class="landing">
                 <div class="color">
                     <h1 class="display-1 pt-5 title pt-5">Ryan Wilhelm</h1>
-                    <p class="text-light h2 slogan">Web Development Re<span class="h1 imagine">IMAGINED</span></p>
+                    <p class="text-light h2 slogan">Web Development Re<span class="h2 imagine">IMAGINED</span></p>
                 </div>
             </div>
         )
