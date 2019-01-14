92da1055769ac6ab77dab2dd87b7a0ec38ee6031
Ryan <ryanmwb@gmail.com>
2019-01-13 20:14:21 -0500

    slogan styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,7 +5,7 @@ class Landing extends Component {
         return(
             <div class="landing">
                 <div class="color">
-                    <h1 class="display-3 pt-5 title pt-5">Ryan Wilhelm</h1>
+                    <h1 class="display-3 pt-5 title ">Ryan Wilhelm</h1>
                     <p class="text-light h2 slogan">Web Development Re<span class="h2 imagine">IMAGINED</span></p>
                 </div>
             </div>
